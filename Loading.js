e93921f5be2d0fef42db834832bafcc77d71781d
zubra5 <larissapflyug@gmail.com>
2016-09-13 11:47:47 +0600

    loading dialog

---
+++ Loading.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function() {
+    // create the loading window and set autoOpen to false
+    $("#loadingScreen").dialog({
+        autoOpen: false,    // set this to false so we can manually open it
+        dialogClass: "loadingScreenWindow",
+        closeOnEscape: false,
+        draggable: false,
+        width: 50,
+        minHeight: 100,
+        modal: true,
+        buttons: {},
+        resizable: false,
+        open: function() {
+            // scrollbar fix for IE
+            $('body').css('overflow','hidden');
+        },
+        close: function() {
+            // reset overflow
+            $('body').css('overflow','auto');
+        }
+    }); // end of dialog
+});
+function waitingDialog() { 
+    $("#loadingScreen").dialog('open');
+}
+function closeWaitingDialog() {
+    $("#loadingScreen").dialog('close');
+}
